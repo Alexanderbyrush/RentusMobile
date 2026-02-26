f82caf27928c7ceaf45dbd12ff39bfeabd683e86
Alexanderbyrush <79385390+Alexanderbyrush@users.noreply.github.com>
2026-02-25 19:04:06 -0500

    Add Compose material icons extended dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation(libs.androidx.compose.foundation)
     implementation(libs.androidx.compose.foundation.layout)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.material.icons.extended)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.navigation.runtime.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
